cc28069293407068718d9d6f1de687a5b79ff648
fwa7son <21fwatson@athenian.org>
2020-09-30 01:27:08 +0000

    completed

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,25 +1,56 @@
+import java.util.Arrays;
 public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int arrlen = arr.length;
+        int arrlen2 = arrlen - 1;
+        int reversed[] = new int[arrlen];
+        for (int  i = 0; i<arrlen; i++){
+            reversed[i] = arr[arrlen2-i];
+        }
+        return reversed;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int arrlen = arr.length;
+        for (int i = 0; i < arrlen; i++){
+            for (int a = i+1; a < arrlen; a++){
+                if (arr[i] > arr[a]){
+                    int x = arr[i];
+                    arr[i] = arr[a];
+                    arr[a] = x;
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+        return arr[arrlen - 2];
     }
 
     // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        boolean IsGeom = false;
+        int arrlen = arr.length;
+        double f = (double)arr[1] / (double)arr[0];
+        for (int i=2;i<arrlen;++i){
+            if ((arr[1] *= f) == arr[i]){
+                IsGeom = true;
+            }
+            else{
+                break;
+            }
+        }
+        return IsGeom;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        int testarr[]= {2, 5, 18, 54};
+        System.out.println(Arrays.toString(reverse(testarr)));
+        System.out.println(secondLargest(testarr));
+        System.out.println(isGeometric(testarr));
     }
 }
